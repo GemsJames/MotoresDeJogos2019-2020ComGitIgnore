b4f12c0bb6f44abe466bbe4741c699928bbb4390
flameoncat <francisco.morim@gmail.com>
2020-02-07 21:34:52 +0000

    reupload funcional

---
+++ Pipeline_test/Pipeline_test/StringConc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pipeline_test
+{
+    static class StringConc
+    {
+        static StringBuilder stringBuilder;
+        public static List<string> stringList;
+
+        public static void Initialize()
+        {
+            stringBuilder = new StringBuilder();
+        }
+ 
+        public static string ConcString()
+        {
+            stringBuilder.Clear();
+            for (int i = 0; i < stringList.Count(); i++)
+            {
+                stringBuilder.Append(stringList[i]);
+            }
+
+            return stringBuilder.ToString();           
+        }
+    }
+}
